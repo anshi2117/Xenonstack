86d3c25c9fad17d63a82ac37d0079ad4a1079914
Anshika Sikarwar <106373368+anshi2117@users.noreply.github.com>
2022-10-19 22:37:52 +0530

    Add files via upload

---
+++ content-option.js
@@ -0,0 +1,54 @@
+const logotext = 'JOHN';
+const meta = {
+  title: 'anshika sikarwar',
+  description:
+    'I’m anshika sikarwar currently pursuing from Chandigarh University',
+};
+
+const introdata = {
+  title: 'I’m anshika sikarwar',
+  animated: {
+    first: 'I love coding',
+    second: 'I code cool websites',
+  },
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum',
+  your_img_url: 'https://images.unsplash.com/photo-1514790193030-c89d266d5a9d',
+};
+
+const dataabout = {
+  title: 'abit about my self',
+  aboutme:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.',
+};
+
+
+
+
+const services = [
+  {
+    title: 'UI & UX Design',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.',
+  },
+  {
+    title: 'Mobile Apps',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.',
+  },
+  {
+    title: 'Wordpress Design',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.',
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: 'anshika.singh2721@gmail.com',
+  YOUR_FONE: '9076757975',
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ',
+  YOUR_SERVICE_ID: 'service_id',
+  YOUR_TEMPLATE_ID: 'template_id',
+  YOUR_USER_ID: 'user_id',
+};
